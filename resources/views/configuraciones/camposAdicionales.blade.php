8a41df8e52325112801246836de4b9f8a05097e7
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-02-21 10:15:37 -0600

    21Febrero_2

--- resources/views/configuraciones/camposAdicionales.blade.php
+++ resources/views/configuraciones/camposAdicionales.blade.php
@@ -1,1 +1,12 @@
-campos adicionales
+
+<fieldset class="mt-2 pt-2">
+    <form>
+        <div class="card">
+            <div class="card-title border-bottom">Campos Adicionales</div>
+            <div class="card-body">
+                sada
+            </div>
+          
+        </div>
+    </form>
+</fieldset>
