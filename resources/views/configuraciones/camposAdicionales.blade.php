9c7cef01e6c0127652dc39997b8b27b12660a327
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-02-21 12:34:22 -0600

    Pantalla Adicionales

--- resources/views/configuraciones/camposAdicionales.blade.php
+++ resources/views/configuraciones/camposAdicionales.blade.php
@@ -1,12 +1,69 @@
 
 <fieldset class="mt-2 pt-2">
     <form>
-        <div class="card">
-            <div class="card-title border-bottom">Campos Adicionales</div>
-            <div class="card-body">
-                sada
+        <div class="container">
+            <div class="row">
+                <div class="card ml-1 mt-1 mb-1 pb-1 col-sm">
+                    <label for="exampleFormControlSelect1" class="border-bottom">Campos Adicionales</label>
+                    <select multiple="" class="form-control form-control-sm" id="exampleFormControlSelect1">
+                        <option>RFC</option>
+                        <option>R&eacute;gimen Fiscal</option>
+                        <option>Colonia</option>
+                        <option>ID Cliente</option>
+                        <option>Usuario Webservice</option>
+                        <option>Contrase&ntilde;a Webservice</option>
+                    </select>
+                    <small id="exampleFormControlSelect1" class="form-text text-muted">
+                        Doble clic para editar un campo
+                    </small>
+                </div>
             </div>
-          
+            <div class="row">
+                <div class="card ml-1 mt-1 mb-1 pb-1 col-sm">
+                    <label class="border-bottom">Agregar un nuevo campo:</label>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <label for="conf_datosAdicionales_nombre">Nombre del campo</label>
+                            <input id="conf_datosAdicionales_nombre" type="text" class="form-control form-control-sm" >
+                        </div>
+                        <div class="col-sm-4">
+                            <label for="conf_datosAdicionales_tipoDato">Tipo de dato</label>
+                            <select id="conf_datosAdicionales_tipoDato" class="form-control form-control-sm">
+                                <option>Num&eacute;rico entero</option>
+                                <option>Num&eacute;rico flotante</option>
+                                <option>Alfanum&eacute;rico</option>
+                                <option>Alfabetico</option>
+                                <option>Fecha</option>
+                            </select>
+                        </div>
+                        <div class="col-sm-4">
+                            <label for="conf_datosAdicionales_seccion">Secci&oacute;n</label>
+                            <select id="conf_datosAdicionales_seccion" class="form-control form-control-sm">
+                                <option>Prospectos/Alta</option>
+                                <option>Prospectos/Edici&oacute;n</option>
+                                <option>Clientes/Alta</option>
+                                <option>Clientes/Edici&oacute;n</option>
+                                <option>Productos/Alta</option>
+                                <option>Productos/Edici&oacute;n</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col-sm-4">
+                            <label for="conf_datosAdicionales_obligatorio">Obligatorio</label>
+                            <input id="conf_datosAdicionales_obligatorio" type="checkbox" class="form-control form-control-sm" >
+                        </div>
+                        <div class="col-sm-4">
+                            <label for="conf_datosAdicionales_validacion">Validaci&oacute;n (Expresi&oacute;n Regular)</label>
+                            <input id="conf_datosAdicionales_validacion" type="text" class="form-control form-control-sm" >
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br />
+        <div class="col-xs-1" align="center">
+            <button type="button" class="btn btn-sm btn-success">Guardar Campo</button>
         </div>
     </form>
 </fieldset>
--- resources/views/configuraciones/smtp.blade.php
+++ resources/views/configuraciones/smtp.blade.php
@@ -1,53 +1,56 @@
 
 <fieldset class="mt-2 pt-2">
     <form>
-        
-        <div class="card pt-2 pb-2 container">
+        <div class="container">
             <div class="row">
-                <div class="col-sm-3">
-                    <label for="conf_smtp_host">Servidor SMTP (host)</label>
-                    <input type="text" class="form-control form-control-sm" id="conf_smtp_host" >
-                </div>
-                <div class="col-sm-3">
-                    <label for="conf_smtp_usuario">Usuario</label>
-                    <input type="text" class="form-control form-control-sm" id="conf_smtp_usuario" >
-                </div>
-                <div class="col-sm-3">
-                    <label for="conf_smtp_passwd">Contrase&ntilde;a</label>
-                    <input type="text" class="form-control form-control-sm" id="conf_smtp_passwd" >
-                </div>
-                <div class="col-sm-3">
-                    <label for="conf_smtp_port">Puerto</label>
-                    <input type="text" class="form-control form-control-sm" id="conf_smtp_port" >
+                <div class="card ml-1 mt-1 mb-1 pb-1 col-sm">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <label for="conf_smtp_host">Servidor SMTP (host)</label>
+                            <input type="text" class="form-control form-control-sm" id="conf_smtp_host" >
+                        </div>
+                        <div class="col-sm-3">
+                            <label for="conf_smtp_usuario">Usuario</label>
+                            <input type="text" class="form-control form-control-sm" id="conf_smtp_usuario" >
+                        </div>
+                        <div class="col-sm-3">
+                            <label for="conf_smtp_passwd">Contrase&ntilde;a</label>
+                            <input type="text" class="form-control form-control-sm" id="conf_smtp_passwd" >
+                        </div>
+                        <div class="col-sm-3">
+                            <label for="conf_smtp_port">Puerto</label>
+                            <input type="text" class="form-control form-control-sm" id="conf_smtp_port" >
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <label for="conf_smtp_security">Seguridad</label>
+                            <select class="custom-select custom-select-sm" id="conf_smtp_security">
+                                <option>Ninguna</option>
+                                <option>TLS</option>
+                                <option>SSL</option>
+                            </select>
+                        </div>
+                        <div class="col-sm-3">
+                            <label for="conf_smtp_from">De</label>
+                            <input type="text" class="form-control form-control-sm" id="conf_smtp_from" >
+                        </div>
+                        <div class="col-sm-3">
+                            <label for="conf_smtp_copy">Copia</label>
+                            <input type="text" class="form-control form-control-sm" id="conf_smtp_copy" >
+                        </div>
+                        <div class="col-sm-3">
+                            <label for="conf_smtp_passwd">Contrase&ntilde;a</label>
+                            <input type="text" class="form-control form-control-sm" id="conf_smtp_passwd" >
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-sm-3">
-                    <label for="conf_smtp_security">Seguridad</label>
-                    <select class="custom-select custom-select-sm" id="conf_smtp_security">
-                        <option>Ninguna</option>
-                        <option>TLS</option>
-                        <option>SSL</option>
-                    </select>
-                </div>
-                <div class="col-sm-3">
-                    <label for="conf_smtp_from">De</label>
-                    <input type="text" class="form-control form-control-sm" id="conf_smtp_from" >
-                </div>
-                <div class="col-sm-3">
-                    <label for="conf_smtp_copy">Copia</label>
-                    <input type="text" class="form-control form-control-sm" id="conf_smtp_copy" >
-                </div>
-                <div class="col-sm-3">
-                    <label for="conf_smtp_passwd">Contrase&ntilde;a</label>
-                    <input type="text" class="form-control form-control-sm" id="conf_smtp_passwd" >
-                </div>
+            <br />
+            <div class="col-xs-1" align="center">
+                <button type="button" class="btn btn-sm btn-primary">Probar Conexi&oacute;n</button>
+                <button type="button" class="btn btn-sm btn-success disabled">Guardar Configuraci&oacute;n</button>
             </div>
-        </div>
-        <br />
-        <div class="col-xs-1" align="center">
-            <button type="button" class="btn btn-sm btn-primary">Probar Conexi&oacute;n</button>
-            <button type="button" class="btn btn-sm btn-success disabled">Guardar Configuraci&oacute;n</button>
         </div>
     </form>
 </fieldset>
