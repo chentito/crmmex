e018698ef3dbcdec043394dfd508c65b2a4e7610
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-03-20 13:38:41 -0600

    20Mar_2

--- resources/views/crm/productos/productoAlta.blade.php
+++ resources/views/crm/productos/productoAlta.blade.php
@@ -5,7 +5,65 @@
 @endsection
 
 @section( 'seccionContenido' )
-ALTA DE PRODUCTO
+<div id="app">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card card-small">
+                <div class="card-header border-bottom"><h6 class="m-0">Datos Generales</h6></div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-8 mb-1">
+                            <input type="text" class="form-control form-control-sm" placeholder="Nombre del producto">
+                        </div>
+                        <div class="col-sm-4 mb-1">
+                            <combo-box id-cat="9" nombre-select="combo_giro"></combo-box>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-3 mb-1">
+                            <combo-box id-cat="6" nombre-select="combo_giro"></combo-box>
+                        </div>
+                        <div class="col-sm-3 mb-1">
+                            <combo-box id-cat="1" nombre-select="combo_giro"></combo-box>
+                        </div>
+                        <div class="col-sm-3 mb-1">
+                            <combo-box id-cat="8" nombre-select="combo_giro"></combo-box>
+                        </div>
+                        <div class="col-sm-3 mb-1">
+                            <input type="text" class="form-control form-control-sm" placeholder="">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <textarea class="form-control form-control-sm" rows="2" placeholder="Observaciones"></textarea>
+                        </div>
+                    </div>
+                    <div class="card-header border-bottom mb-1"><h6 class="m-0">Precios</h6></div>
+                    <div class="row">
+                        <div class="col-sm-3 input-group mb-1">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">$</span>
+                            </div>
+                            <input type="text" class="form-control form-control-sm" aria-label="Cuota de alta" value="">
+                        </div>
+                        <div class="col-sm-3 input-group mb-1">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">$</span>
+                            </div>
+                            <input type="text" class="form-control form-control-sm" aria-label="Precio recurrente" value="">
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <boton-regresar></boton-regresar>
+                    <div class="float-right">
+                        <button class="btn btn-sm btn-outline-accent"><i class="material-icons">add</i> Agregar Producto</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section( 'seccionFooter' )
