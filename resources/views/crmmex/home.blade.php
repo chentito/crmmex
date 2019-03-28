1bd993b33920c6c80a630fb7b604157cc2c0d43a
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-03-28 11:16:43 -0600

    Fonts

--- resources/views/crmmex/home.blade.php
+++ resources/views/crmmex/home.blade.php
@@ -2,5 +2,4 @@
 @extends( 'crmmex.index' , [ 'estilo' => $estilo , 'css' => $css , 'btn' => $btn ] )
 
 @section( 'contenidos' )
-   
 @endsection
--- resources/views/crmmex/utils/menu.blade.php
+++ resources/views/crmmex/utils/menu.blade.php
@@ -16,7 +16,7 @@
         </ul>
     </li>
     <li>
-        <a href="#mercadotecnia" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-user fa-lg"></i>  Mercadotecnia</a>
+        <a href="#mercadotecnia" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-lightbulb fa-lg"></i>  Mercadotecnia</a>
         <ul class="collapse list-unstyled" id="mercadotecnia">
             <li><a href="javascript:void(0)">Mercadotecnia</a></li>
         </ul>
