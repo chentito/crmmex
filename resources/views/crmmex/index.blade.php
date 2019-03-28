b3724ad1289b2b5da89952750038c34328ca898e
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-03-28 17:58:02 -0600

    AjustesMenuHeader

--- resources/views/crmmex/index.blade.php
+++ resources/views/crmmex/index.blade.php
@@ -42,26 +42,27 @@
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav ml-auto">
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="/home4">CRM <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link" href="/home4">Dashboard<span class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Avisos
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item">MSJ</a>
-                                    </div>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <a href="#" data-toggle="dropdown" aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle">
                                         Bienvenido Carlos Reyes
                                     </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="z-index: 1500">
-                                        <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Something else here</a>
-                                    </div>
+                                    <ul class="dropdown-menu collapse list-unstyled {{$borde}}" aria-labelledby="navbarDropdown" style="font-size: 12px">
+                                        <li><a id="menuHeader" class="dropdown-item" href="javascript:void(0)" onclick="return contenidos('prospectos_listado');">Editar Perfil</a></li>
+                                        <li><a class="dropdown-item" href="javascript:void(0)" onclick="return contenidos('prospectos_nuevo');">Actividades</a></li>
+                                        <li><a class="dropdown-item" href="javascript:void(0)" onclick="return contenidos('prospectos_seguimiento');">Reportes</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a href="#" data-toggle="dropdown" aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle">
+                                        Alertas
+                                    </a>
+                                    <ul class="dropdown-menu collapse list-unstyled {{$borde}}" aria-labelledby="navbarDropdown" style="font-size: 12px">
+                                        <li><a id="menuHeader" class="dropdown-item" href="javascript:void(0)" onclick="return contenidos('prospectos_listado');">Editar Perfil</a></li>
+                                        <li><a class="dropdown-item" href="javascript:void(0)" onclick="return contenidos('prospectos_nuevo');">Actividades</a></li>
+                                        <li><a class="dropdown-item" href="javascript:void(0)" onclick="return contenidos('prospectos_seguimiento');">Reportes</a></li>
+                                    </ul>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('logout') }}"
