15294b3e536de2d15e276586b597c17dc1d7c393
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-03-28 14:36:00 -0600

    Ajustes header

--- resources/views/crmmex/index.blade.php
+++ resources/views/crmmex/index.blade.php
@@ -32,52 +32,48 @@
             <!-- Page Content -->
             <div class="container-fluid" id="content">
                 <header id="header" class="bg-light sticky-top">
-                    <div class="container-fluid">
-                        <button type="button" id="sidebarCollapse" class="btn {{$btn}} mt-2">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light" >
+                        <button type="button" id="sidebarCollapse" class="btn {{$btn}} mt-1">
                             <i class="fas fa-align-left"></i>
                         </button>
-                        <div class="float-right">
-                            <!--nav class="navbar navbar-expand-lg navbar-light bg-light" >
-                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon"></span>
-                                </button>
-                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <ul class="navbar-nav mr-auto">
-                                        <li class="nav-item active">
-                                            <a class="nav-link" href="/home4">CRM <span class="sr-only">(current)</span></a>
-                                        </li>
-                                        <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                Avisos
-                                            </a>
-                                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                <a class="dropdown-item">MSJ</a>
-                                            </div>
-                                        </li>
-                                        <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                Bienvenido Carlos Reyes
-                                            </a>
-                                            <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="z-index: 1500">
-                                                <a class="dropdown-item" href="#">Action</a>
-                                                <a class="dropdown-item" href="#">Another action</a>
-                                                <div class="dropdown-divider"></div>
-                                                <a class="dropdown-item" href="#">Something else here</a>
-                                            </div>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="{{ route('logout') }}"
-                                               onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                                Cerrar Sesi&oacute;n
-                                            </a>
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;"> @csrf </form>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </nav-->
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="/home4">CRM <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Avisos
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item">MSJ</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Bienvenido Carlos Reyes
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="z-index: 1500">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                        Cerrar Sesi&oacute;n
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;"> @csrf </form>
+                                </li>
+                            </ul>
                         </div>
-                    </div>
+                    </nav>
                 </header>
                 <div style="height: 35px" style="z-index: 1400" id="contenedorBreadCrumb">
                     <ol class="breadcrumb rounded-0 {{$estilo}} shadow">
@@ -97,6 +93,7 @@
                 </div>
             </div>
         </div>
+
         <div class="modal fade bd-example-modal-lg" data-backdrop="static" data-keyboard="false" tabindex="-1">
             <div class="modal-dialog modal-sm">
                 <div class="modal-content" style="width: 48px">
@@ -104,7 +101,9 @@
                 </div>
             </div>
         </div>
+
         <div id="app"></div>
+
         <!-- SCRIPTS -->
         <script src="{{ asset( 'js/app.js' ) }}"></script>
         <script src="{{ asset( 'assets2/js/popper.js' ) }}"></script>
@@ -121,6 +120,7 @@
         <script src="{{ asset( 'assets3/js/fontawesome.js' ) }}"></script>
         <!-- Funcionalidades -->
         <script src="{{ asset( 'assets3/js/contenidos.js' ) }}"></script>
+
         <script type="text/javascript">
             $(document).ready(function () {
                 $('#sidebarCollapse').on('click', function () {
@@ -132,5 +132,6 @@
             });
             contenidos( 'dashboard' );
         </script>
+
     </body>
 </html>
