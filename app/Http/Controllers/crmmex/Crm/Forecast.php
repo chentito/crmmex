8ea3e96e8083ca3c133907d21947a7f2fd8af5e2
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-06-03 16:31:37 -0500

    PipelineYForecast

---
+++ app/Http/Controllers/crmmex/Crm/Forecast.php
@@ -0,0 +1,16 @@
+<?php
+/*******************************************************************************
+ * Controlador que recaba todos los datos necesarios para el calculo de forecas
+ * @Autor Mexagon.net / Carlos Reyes
+ * @Fecha Mayo 2019
+ ******************************************************************************/
+
+namespace App\Http\Controllers\crmmex\Crm;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Forecast extends Controller
+{
+    //
+}
