f5212b22d2a7c221ce039879b08630bad7efb83e
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-06-18 16:18:44 -0500

    2

---
+++ resources/views/crmmex/configuraciones/destinatarios.blade.php
@@ -0,0 +1,10 @@
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Listas Destinatarios</a>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+      Se aran consultas de destinatarios, basados en parametros especificos del listado de clientes
+  </div>
+</div>
