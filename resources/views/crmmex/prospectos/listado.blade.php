a759507028f21651361522778db6089e4f588902
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-06-20 18:57:10 -0500

    ultimo_20junio

--- resources/views/crmmex/prospectos/listado.blade.php
+++ resources/views/crmmex/prospectos/listado.blade.php
@@ -1,13 +1,5 @@
-<div class="card card-small w-100">
-    <div class="card-body">
-      <div id="listadoProspectos_config"></div>
-      <table id="listadoProspectos" class="table table-striped table-bordered" style="width:100%">
-      </table>
-    </div>
-</div>
 
-<script>
-    $(document).ready( function() {
-        generaDataGrid( 'listadoProspectos' );
-    });
-</script>
+<div id="listadoProspectos_config"></div>
+<table id="listadoProspectos" class="table table-striped table-bordered" style="width:100%"></table>
+
+<script> generaDataGrid( 'listadoProspectos' ); </script>
