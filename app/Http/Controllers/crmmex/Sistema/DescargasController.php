df7d168e32cba8c72b773770c037ecdc3940c10c
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-08-13 17:51:34 -0500

    download

--- app/Http/Controllers/crmmex/Sistema/DescargasController.php
+++ app/Http/Controllers/crmmex/Sistema/DescargasController.php
@@ -12,7 +12,7 @@ class DescargasController extends Controller
   public function descargaArchivoEjemplo( $archivo ) {
 
     switch( $archivo ) {
-      case 'LayoutProductos' : $file = public_path() . '\storage\ejemplos\EjemploLayoutProductos.csv'; break;
+      case 'LayoutProductos' : $file = public_path() . '/storage/ejemplos/EjemploLayoutProductos.csv'; break;
     }
 
     $headers = array( 'Content-Type: application/csv' );
